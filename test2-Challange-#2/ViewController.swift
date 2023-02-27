357bae9dd93c71fdce01c43b398ff2181583ffbc
Mihail-DV <76704615+Mihail-DV@users.noreply.github.com>
2023-02-27 17:10:55 +0300

    Initial Commit

---
+++ test2-Challange-#2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test2-Challange-#2
+//
+//  Created by Михаил Данилин on 27.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
